925d729fae11aafa4ee04b32634ac0d120423835
Horsev <andrey.horsev@gmail.com>
2024-02-10 16:13:54 +0200

    Linter added

--- app.js
+++ app.js
@@ -1,24 +1,21 @@
-const express = require("express");
+import express, { static as staticFolder } from "express";
+
+const { log } = console;
+
 const app = express();
 
 app.set("view engine", "pug");
 
-app.use(express.static("public"));
+app.use(staticFolder("public"));
 
 app.get("/", (req, res) => {
-  res.render("index.pug", {
-    title: "Home Page",
-  });
+  res.render("index.pug", { title: "Home Page" });
 });
 
 app.get("/about", (req, res) => {
-  res.render("about.pug", {
-    title: "About Page",
-  });
+  res.render("about.pug", { title: "About Page" });
 });
 
 app.listen(8699, () => {
-  console.log(
-    "Server is running on 8699"
-  );
+  log("Server is running on 8699");
 });
