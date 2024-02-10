517009cb97109d518b4afe653b6dc970b91f9438
Horsev <andrey.horsev@gmail.com>
2024-02-10 15:41:36 +0200

    Init

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.set('view engine', 'pug');
+app.set("view engine", "pug");
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-    res.render('index.pug', {
-        title: 'Home Page'
-    });
+app.get("/", (req, res) => {
+  res.render("index.pug", {
+    title: "Home Page",
+  });
 });
-app.get('/about', (req, res) => {
-    res.render('about.pug', {
-        title: 'About Page'
-    });
+
+app.get("/about", (req, res) => {
+  res.render("about.pug", {
+    title: "About Page",
+  });
 });
+
 app.listen(8699, () => {
-    console.log('Server is running on 8699');
-
-})
+  console.log(
+    "Server is running on 8699"
+  );
+});
