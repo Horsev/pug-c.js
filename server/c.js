ebdc4c9245cefbf796f78c854936e2c6896d6af6
Horsev <andrey.horsev@gmail.com>
2024-02-11 09:30:54 +0200

    Template tuning

--- server/c.js
+++ server/c.js
@@ -6,20 +6,14 @@ const { APIKEY, API_DOMAIN, API_PATH } = process.env;
 const endpointCompany = (code) =>
   `${API_DOMAIN}${API_PATH}/${code}?apiKey=${APIKEY}`;
 
-const getData = (response) => response.json();
-
-const mapData =
-  (res, mapper) =>
-  ({ data }) => {
-    res.render("c.pug", mapper(data));
-  };
-
 const createCPage = async (req, res) => {
   const { code } = req.params;
 
-  await fetch(endpointCompany(code))
-    .then(getData)
-    .then(mapData(res, fullCompanyMapper));
+  const endpoint = endpointCompany(code);
+
+  const response = await fetch(endpoint);
+  const { data } = await response.json();
+  res.render("c.pug", fullCompanyMapper(data));
 };
 
 export default createCPage;
